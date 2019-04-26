572a5284ab2e33f098b7e1e5915190e1a4e34de5
Pavel Smirnov <smirnovpp@yandex-team.ru>
2019-04-26 15:10:31 +0300

    Updates

---
+++ precache-manifest.6bbcc537ff4158eb4a219d60fb60b658.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "4bb51bc4fe59c5f1e580",
+    "url": "/picsearch-talk-demo/static/js/runtime~main.5cfc2267.js"
+  },
+  {
+    "revision": "b1185e8e75b96cf05c5b",
+    "url": "/picsearch-talk-demo/static/js/main.ec492ba3.chunk.js"
+  },
+  {
+    "revision": "39abe871db87baea49fd",
+    "url": "/picsearch-talk-demo/static/js/2.747e81eb.chunk.js"
+  },
+  {
+    "revision": "b1185e8e75b96cf05c5b",
+    "url": "/picsearch-talk-demo/static/css/main.5d6e424d.chunk.css"
+  },
+  {
+    "revision": "59ee14b85ee15596e436d2504e358e96",
+    "url": "/picsearch-talk-demo/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/picsearch-talk/precache-manifest.be765e020d71e96e13925a7a6f4bc1c3.js"
+  "/picsearch-talk-demo/precache-manifest.6bbcc537ff4158eb4a219d60fb60b658.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/picsearch-talk/index.html", {
+workbox.routing.registerNavigationRoute("/picsearch-talk-demo/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
